a4460a9fa3b242b90873b4fd8939fc00cb772035
Steven Anderson <thephdgaming@gmail.com>
2015-10-16 11:49:09 -0400

    Fully defined some fields and added correct multiple settings

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,9 +1,9 @@
 class Collection < Sufia::Collection
-  property :institution, predicate: ::RDF::URI.new("http://dbpedia.org/ontology/institution") do |index|
+  property :institution, predicate: ::RDF::URI.new("http://dbpedia.org/ontology/institution"), multiple: false do |index|
       index.as :stored_searchable, :facetable
   end
 
-  property :contact_person, predicate: ::RDF::URI.new("http://digitaltransgenderarchive.net/ns/contactPerson") do |index|
+  property :contact_person, predicate: ::RDF::URI.new("http://digitaltransgenderarchive.net/ns/contactPerson"), multiple: false do |index|
     index.type :text
     index.as :stored_searchable
   end
