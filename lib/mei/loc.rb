d8f58d8bcf7f5197d6b077547925e308d74df9da
Steven Anderson <sanderson@bpl.org>
2015-11-13 08:36:39 -0500

    Updates to the Metadata Enrichment Interface. (Need to clean up soon). Fixes to a few labels

--- lib/mei/loc.rb
+++ lib/mei/loc.rb
@@ -23,9 +23,19 @@ module Mei
 
     # Reformats the data received from the LOC service
     def parse_authority_response
+      threaded_responses = []
+      #end_response = Array.new(20)
+      end_response = []
+      position_counter = 0
       @raw_response.select {|response| response[0] == "atom:entry"}.map do |response|
-        loc_response_to_qa(response_to_struct(response))
+        threaded_responses << Thread.new(position_counter) { |local_pos|
+          end_response[local_pos] = loc_response_to_qa(response_to_struct(response))
+        }
+        position_counter+=1
+        #loc_response_to_qa(response_to_struct(response))
       end
+      threaded_responses.each { |thr|  thr.join }
+      end_response
     end
 
     # Simple conversion from LoC-based struct to QA hash
@@ -35,7 +45,7 @@ module Mei
         json_link = json_link[0][1]
         puts 'Json Link is: ' + json_link
         item_response = get_json(json_link)
-        broader, narrower = get_skos_concepts(item_response)
+        broader, narrower, variants = get_skos_concepts(item_response)
       end
 
       #count = ActiveFedora::Base.find_with_conditions("subject_tesim:#{data.id.gsub('info:lc', 'http://id.loc.gov').gsub(':','\:')}", rows: '100', fl: 'id' ).length
@@ -48,11 +58,14 @@ module Mei
         count = count.to_s
       end
 
+
+
       {
           "uri_link" => data.id.gsub('info:lc', 'http://id.loc.gov') || data.title,
           "label" => data.title,
           "broader" => broader,
           "narrower" => narrower,
+          "variants" => variants,
           "count" => count
       }
     end
@@ -79,31 +92,56 @@ module Mei
     def get_skos_concepts response
       broader_list = []
       narrower_list = []
+      variant_list = []
+
       response.each do |resp|
         if resp.has_key?("http://www.loc.gov/mads/rdf/v1#hasBroaderAuthority")
           resp["http://www.loc.gov/mads/rdf/v1#hasBroaderAuthority"].each do |broader|
-            broader_uri = broader["@id"]
+            if broader["@id"].present?
+              broader_uri = broader["@id"]
 
-            broader_label = response.select { |broader_resp| broader_resp["@id"] == broader_uri and broader_resp.has_key?("http://www.loc.gov/mads/rdf/v1#authoritativeLabel") and !broader_resp.has_key?("http://www.loc.gov/mads/rdf/v1#elementList") }
-            broader_label = broader_label[0]["http://www.loc.gov/mads/rdf/v1#authoritativeLabel"][0]["@value"]
+              broader_label = response.select { |broader_resp| broader_resp["@id"] == broader_uri and broader_resp.has_key?("http://www.loc.gov/mads/rdf/v1#authoritativeLabel") and !broader_resp.has_key?("http://www.loc.gov/mads/rdf/v1#elementList") }
+              broader_label = broader_label[0]["http://www.loc.gov/mads/rdf/v1#authoritativeLabel"][0]["@value"]
 
-            broader_list << {:uri_link=>broader_uri, :label=>broader_label}
+              broader_list << {:uri_link=>broader_uri, :label=>broader_label}
+            end
           end
         end
         if resp.has_key?("http://www.loc.gov/mads/rdf/v1#hasNarrowerAuthority")
           resp["http://www.loc.gov/mads/rdf/v1#hasNarrowerAuthority"].each do |narrower|
-            narrower_uri = narrower["@id"]
+            if narrower["@id"].present?
+              narrower_uri = narrower["@id"]
 
-            narrower_label = response.select { |narrower_resp| narrower_resp["@id"] == narrower_uri and narrower_resp.has_key?("http://www.loc.gov/mads/rdf/v1#authoritativeLabel") and !narrower_resp.has_key?("http://www.loc.gov/mads/rdf/v1#elementList") }
-            narrower_label = narrower_label[0]["http://www.loc.gov/mads/rdf/v1#authoritativeLabel"][0]["@value"]
 
-            narrower_list << {:uri_link=>narrower_uri, :label=>narrower_label}
+              narrower_label = response.select { |narrower_resp| narrower_resp["@id"] == narrower_uri and narrower_resp.has_key?("http://www.loc.gov/mads/rdf/v1#authoritativeLabel") and !narrower_resp.has_key?("http://www.loc.gov/mads/rdf/v1#elementList") }
+              narrower_label = narrower_label[0]["http://www.loc.gov/mads/rdf/v1#authoritativeLabel"][0]["@value"]
+
+              narrower_list << {:uri_link=>narrower_uri, :label=>narrower_label}
+            end
+
+          end
+
+        end
+
+        if resp.has_key?("http://www.loc.gov/mads/rdf/v1#variantLabel")
+          resp["http://www.loc.gov/mads/rdf/v1#variantLabel"].each do |variant|
+            if variant["@value"].present?
+              varient_label = variant["@value"]
+=begin
+              if variant["@language"].present?
+                varient_label += "@#{variant["@language"]}"
+              end
+=end
+
+              variant_list << varient_label
+            end
+
           end
 
         end
       end
 
-      return broader_list, narrower_list
+      return broader_list, narrower_list, variant_list
     end
   end
 end
