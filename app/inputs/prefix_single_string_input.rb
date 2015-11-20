ed4611adc3a5f73ab31b787fd8e29fa1caeea966
Steven Anderson <thephdgaming@gmail.com>
2015-11-20 09:02:58 -0500

    Helps text updated for the generic files upload. Added icons to the dashboard for institutions and homosaurus.

---
+++ app/inputs/prefix_single_string_input.rb
@@ -0,0 +1,19 @@
+class PrefixSingleStringInput < SingleBaseInput
+  include WithHelpIcon
+
+  def inner_wrapper
+    <<-HTML
+          <li class="field-wrapper">
+             <div class="input-group col-sm-12">
+               <span class="input-group-addon">http://www.homosaurus.org/terms/</span>
+              #{yield}
+              </div>
+          </li>
+    HTML
+  end
+
+  def input_type
+    #'multi_value'.freeze
+    'no_repeat_field_value'.freeze
+  end
+end
