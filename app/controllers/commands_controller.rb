f3d01bd71987036ad89bffd464d0d2db4ad86b9c
Steven Anderson <sanderson@bpl.org>
2015-12-03 08:40:27 -0500

    Full re-checkin of current code

--- app/controllers/commands_controller.rb
+++ app/controllers/commands_controller.rb
@@ -2,11 +2,18 @@ class CommandsController < ApplicationController
   #before_action :verify_admin
 
   def update
+=begin
     stdin, stdout, stderr = Open3.popen3('git pull origin master')
     stdin, stdout, stderr = Open3.popen3('bundle update')
     stdin, stdout, stderr = Open3.popen3('bundle exec rake assets:precompile --trace RAILS_ENV=production')
     stdin, stdout, stderr = Open3.popen3('bundle exec rake db:migrate RAILS_ENV="production"')
     stdin, stdout, stderr = Open3.popen3('touch tmp/restart.txt')
+=end
+`git pull origin master`
+`bundle update`
+`bundle exec rake assets:precompile --trace RAILS_ENV=production`
+`bundle exec rake db:migrate RAILS_ENV="production"`
+`touch tmp/restart.txt`
 
     respond_to do |format|
       format.html { render :text => "Updated. Application should restart soon." }
