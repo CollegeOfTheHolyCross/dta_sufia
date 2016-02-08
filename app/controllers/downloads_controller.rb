789b15f5d92b5ee898b7d67e2672d21ac22f3655
Steven Anderson <sanderson@bpl.org>
2016-02-07 23:13:43 -0500

    Fix to view thumbnail of private item

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,6 +1,14 @@
 class DownloadsController < ApplicationController
   include Sufia::DownloadsControllerBehavior
 
+  def authorize_download!
+    if params["file"].present? and params["file"] == 'thumbnail'
+      return true
+    else
+      authorize! :download, file
+    end
+  end
+
   #FIXME: This is cached in sufia... means that users can't see files changed sometimes...
   def file
     #load_file
