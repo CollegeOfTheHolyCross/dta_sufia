645a9df7bad46b689767d125b7c9cfe6e3369f11
Steven Anderson <sanderson@bpl.org>
2016-02-24 08:39:53 -0500

    Attempt to fix search builder

--- lib/dta_search_builder.rb
+++ lib/dta_search_builder.rb
@@ -10,15 +10,15 @@ module DtaSearchBuilder
   end
 
 # used by InstitutionsController#index
-def institutions_filter(solr_parameters = {}, wtf=nil)
-  solr_parameters[:fq] ||= []
-  solr_parameters[:fq] << "+active_fedora_model_ssi:\"Institution\""
-end
+  def institutions_filter(solr_parameters = {}, wtf=nil)
+    solr_parameters[:fq] = []
+    solr_parameters[:fq] << "+active_fedora_model_ssi:\"Institution\""
+  end
 
 # used by CollectionsController#public_index
-def collections_filter(solr_parameters = {}, wtf=nil)
-  solr_parameters[:fq] ||= []
-  solr_parameters[:fq] << "+active_fedora_model_ssi:\"Collection\""
-  solr_parameters[:fq] << "+is_public_ssi:\"true\""
-end
+  def collections_filter(solr_parameters = {}, wtf=nil)
+    solr_parameters[:fq] = []
+    solr_parameters[:fq] << "+active_fedora_model_ssi:\"Collection\""
+    solr_parameters[:fq] << "+is_public_ssi:\"true\""
+  end
 end
