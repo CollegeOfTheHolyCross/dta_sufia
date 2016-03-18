bc65792c8fe9f2d14f459be79f872ef01785a67d
Steven Anderson <sanderson@bpl.org>
2016-03-18 00:54:45 -0400

    More retry added

--- lib/internet_archive/dta_books.rb
+++ lib/internet_archive/dta_books.rb
@@ -12,7 +12,7 @@ module InternetArchive
       raise ArgumentError, 'HTTP redirect too deep' if limit == 0
 
       url = URI.parse(uri_str)
-      req = Net::HTTP::Get.new(url.path, { 'User-Agent' => 'Mozilla/5.0 (etc...)' })
+      req = Net::HTTP::Get.new(url.to_s, { 'User-Agent' => 'Mozilla/5.0 (etc...)' })
       response = Net::HTTP.start(url.host, url.port) { |http| http.request(req) }
       case response
         when Net::HTTPSuccess     then response
