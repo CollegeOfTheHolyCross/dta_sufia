8c905210dfb494afd54e3a21fe1965b1d7b293f1
Steven Anderson <sanderson@bpl.org>
2016-08-05 01:46:25 -0400

    See if mounting oai endpoint earlier than the sufia engine allows it to work...

--- config/routes.rb
+++ config/routes.rb
@@ -66,12 +66,13 @@ Rails.application.routes.draw do
 
   # recently added browse
 
-
+  resources 'oai',
+            only: [:index]
+  
   # This must be the very last route in the file because it has a catch-all route for 404 errors.
     # This behavior seems to show up only in production mode.
     mount Sufia::Engine => '/'
-  resources 'oai',
-            only: [:index]
+
   root to: 'homepage#index'
 
 
