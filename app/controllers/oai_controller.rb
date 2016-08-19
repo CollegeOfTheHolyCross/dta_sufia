253d46fc4e5bd339df2227c53826fec3bfc73dc7
Steven Anderson <sanderson@bpl.org>
2016-08-19 09:02:13 -0400

    Add support for GetRecord in OAI endpoint and autofocus cursor in the Metadata Enrichment Interface

--- app/controllers/oai_controller.rb
+++ app/controllers/oai_controller.rb
@@ -5,7 +5,7 @@ class OaiController < ApplicationController
 
   def index
     @verb = params.delete(:verb)
-    raise("Unsupported verb: #{@verb}") unless @verb == 'ListRecords' || @verb == 'ListSets'
+    raise("Unsupported verb: #{@verb}") unless @verb == 'ListRecords' || @verb == 'ListSets' || @verb == 'GetRecord'
 
     @metadata_prefix = params.delete(:metadataPrefix) || 'dta_dc'
     raise("Unsupported metadataPrefix: #{@metadata_prefix}") unless @metadata_prefix == 'dta_dc'
@@ -13,6 +13,8 @@ class OaiController < ApplicationController
     resumption_token = params.delete(:resumptionToken) || '0'
     raise("Unsupported resumptionToken: #{resumption_token}") unless resumption_token =~ /^\d*$/
     @start = resumption_token.to_i
+
+    identifier = params.delete(:identifier) || ''
 
     unsupported = params.keys - %w(action controller format)
     raise("Unsupported params: #{unsupported}") unless unsupported.empty?
@@ -23,6 +25,11 @@ class OaiController < ApplicationController
       self.list_records
     elsif @verb == 'ListSets'
       self.list_sets
+    elsif @verb == 'GetRecord'
+      if identifier.blank?
+        raise("Requested GetRecord but did not provide an identifier parameter.")
+      end
+      self.get_single_record(identifier)
     end
 
 
@@ -46,7 +53,7 @@ class OaiController < ApplicationController
         end
 
     # Not ideal: they'll need to go past the end.
-    @next_resumption_token = @start + ROWS unless @records.empty?
+    @next_resumption_token = @start + ROWS unless @records.empty? || @records.length < 100
 
     respond_to do |format|
       format.xml do
@@ -73,11 +80,38 @@ class OaiController < ApplicationController
         end
 
     # Not ideal: they'll need to go past the end.
-    @next_resumption_token = @start + ROWS unless @records.empty?
+    @next_resumption_token = @start + ROWS unless @records.empty? || @records.length < 100
 
     respond_to do |format|
       format.xml do
         render :template => "oai/list_sets"
+      end
+    end
+  end
+
+  def get_single_record pid
+    @records =
+        RSolr.connect(url: "#{ActiveFedora.config.credentials[:url].gsub('/fedora/rest', '/solr/')}")
+            .get('select', params: {
+                'q' => 'is_public_ssi:true AND active_fedora_model_ssi:GenericFile AND id:' + pid,
+                #'fl' => 'id,timestamp,xml',
+                'rows' => ROWS,
+                'start' => @start
+            })['response']['docs'].map do |d|
+          ListRecord.new(
+              d['id'],
+              d['timestamp'],
+              #PBCore.new(d['xml'])
+              GenericFile.find(d['id'])
+          )
+        end
+
+    # Not ideal: they'll need to go past the end.
+    @next_resumption_token = @start + ROWS unless @records.empty? || @records.length < 100
+
+    respond_to do |format|
+      format.xml do
+        render :template => "oai/get_record"
       end
     end
   end
